66e79e2a749d2abd1085ecd242cd03a277a769d6
Diptanjankar82 <diptanjankar36@gmail.com>
2024-02-09 23:43:30 +0530

    diptanjan changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int ans2 = p.product(5,2);
     }
 
-   static class Product{
+   public static class Product{
        public int product(int x, int y){
             return x*y;
        }
